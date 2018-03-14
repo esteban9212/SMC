10938d6adedf70586686af1d50fd2d11d44e97ae
Esteban Aguirre Patiño <esteban.aguirre9212@gmail.com>
2018-03-14 10:44:08 -0500

    modelos y migracion tipoestado

--- config/app.php
+++ config/app.php
@@ -140,6 +140,7 @@ return [
         /*
          * Laravel Framework Service Providers...
          */
+
         Illuminate\Auth\AuthServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
@@ -176,7 +177,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        Reliese\Coders\CodersServiceProvider::class
     ],
 
     /*
